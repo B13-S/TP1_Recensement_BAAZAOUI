ba49836353b806003421d8cf08ca51ae065a88bf
B13-S <seifeddine_baazaoui@etu.u-bourgogne.fr>
2024-12-05 23:55:37 +0100

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,69 +1,107 @@
 import csv
+import matplotlib.pyplot as plt
 
-table2008=[]
-donne2008=[]
-table2016=[]
-table2021=[]
-table2023=[]
-donneesFiltrer2008=[]
-donneesFiltrer2016=[]
-donneesFiltrer2021=[]
-donneesFiltrer2023=[]
-codeCommune=[]
-CommuneVisser= ['Appoigny', 'Auxerre','Moneteau', 'Saint-Georges-sur-Baulche']
-populationsTotale08=0
-populationsTotale16=0
-populationsTotale21=0
-populationsTotale23=0
+# communes ciblées
+communes_immediate = ['Appoigny', 'Auxerre', 'Moneteau', 'Saint-Georges-sur-Baulche']
+communes_totale = [
+    'Appoigny', 'Augy', 'Auxerre', 'Bleigny-le-Carreau', 'Branches',
+    'Champs-sur-Yonne', 'Charbuy', 'Chevannes', 'Chitry', 'Coulanges-la-Vineuse',
+    'Escamps', 'Escolives Sainte-Camille', 'Gurgy', 'Gy-l’Evêque', 'Irancy',
+    'Jussy', 'Lindry', 'Moneteau', 'Montigny-la-Resle', 'Perrigny', 'Quenne',
+    'Saint-Bris-le-Vineux', 'Saint-Georges-sur-Baulche', 'Vallan', 'Venoy',
+    'Villefargeau', 'Villeneuve-Saint-Salves', 'Vincelles', 'Vincelottes'
+]
 
-with open('donnees_2008.csv',newline="") as csvfile:
-    reader = csv.reader(csvfile,delimiter=',')
-    for row in reader:
-        table2008.append(row)
-        if row[6] in CommuneVisser:
-            donneesFiltrer2008.append(row)
-            populationsTotale08+=int(row[9])
-        
-with open('donnees_2016.csv',newline="") as csvfile:
-    reader = csv.reader(csvfile,delimiter=',')
-    for row in reader:
-        table2016.append(row)
-        if row[6] in CommuneVisser:
-                donneesFiltrer2016.append(row)
-                populationsTotale16+=int(row[9])
+# initialisation du dico pour stocker les résultats du comptage du populations
+populations = {
+    2008: {"immediate": 0, "totale": 0},
+    2016: {"immediate": 0, "totale": 0},
+    2021: {"immediate": 0, "totale": 0},
+    2023: {"immediate": 0, "totale": 0}
+}
 
+# 2008
+with open('donnees_2008.csv', newline="") as csvfile:
+    csvfile = csv.reader(csvfile, delimiter=',')
+    tete = []  # initialisation l'en-tête
+    for ligne in csvfile:
+        if csvfile.line_num == 1:  # lire la première ligne pour récupurer les noms des colonnes
+            tete = ligne  # enregistrer la résultats
+            col_commune = tete.index('Nom de la commune')
+            col_population = tete.index('Population municipale')
+        else:
+            commune = ligne[col_commune]
+            population = int(ligne[col_population])
 
+            if commune in communes_immediate:
+                populations[2008]['immediate'] += population
+            if commune in communes_totale:
+                populations[2008]['totale'] += population
 
+# 2016
+with open('donnees_2016.csv', newline="") as csvfile:
+    csvfile = csv.reader(csvfile, delimiter=',')
+    tete = []
+    for ligne in csvfile:
+        if csvfile.line_num == 1:
+            tete = ligne
+            col_commune = tete.index('Nom de la commune')
+            col_population = tete.index('Population municipale')
+        else:
+            commune = ligne[col_commune]
+            population = int(ligne[col_population])
 
-with open('donnees_2023.csv',newline="") as csvfile: 
-    reader = csv.reader(csvfile,delimiter=';')
-    for row in reader:
-        table2023.append(row)
-        if row[7] in CommuneVisser:
-            donneesFiltrer2023.append(row)
-            codeCommune.append(row)
-            populationsTotale23+=int(row[10])
+            if commune in communes_immediate:
+                populations[2016]['immediate'] += population
+            if commune in communes_totale:
+                populations[2016]['totale'] += population
 
+# 2021
+with open('donnees_2021.csv', newline="") as csvfile:
+    csvfile = csv.reader(csvfile, delimiter=';')
+    tete = []
+    for ligne in csvfile:
+        if csvfile.line_num == 1:
+            tete = ligne
+            col_commune = tete.index('COM')
+            col_population = tete.index('PMUN')
+        else:
+            commune = ligne[col_commune]
+            population = int(ligne[col_population])
 
-with open('donnees_2021.csv',newline="") as csvfile:
-    reader = csv.reader(csvfile,delimiter=';')
-    for row in reader:
-        table2021.append(row)
-        if row[2] in codeCommune:
-          donneesFiltrer2021.append(row)
-          populationsTotale21+=int(row[5])
+            if commune in communes_immediate:
+                populations[2021]['immediate'] += population
+            if commune in communes_totale:
+                populations[2021]['totale'] += population
 
+# 2023
+with open('donnees_2023.csv', newline="", encoding="utf-8") as csvfile:
+    csvfile = csv.reader(csvfile, delimiter=';')
+    tete = []
+    for ligne in csvfile:
+        if csvfile.line_num == 1:
+            tete = ligne
+            col_commune = tete.index('COM')
+            col_population = tete.index('PMUN')
+        else:
+            commune = ligne[col_commune]
+            population = int(ligne[col_population])
 
-print(codeCommune)
-print("La populqtion de ",CommuneVisser ,"en 2008 est de :",populationsTotale08,"personnes")
-print("La populqtion de ",CommuneVisser ,"en 2016 est de :",populationsTotale16,"personnes")
-print("La populqtion de ",CommuneVisser ,"en 2021 est de :",populationsTotale21,"personnes")
-print("La populqtion de ",CommuneVisser ,"en 2023 est de :",populationsTotale23,"personnes")
-print(donneesFiltrer2008)
-print(donneesFiltrer2016)
-print(donneesFiltrer2021)
-print(donneesFiltrer2023)
+            if commune in communes_immediate:
+                populations[2023]['immediate'] += population
+            if commune in communes_totale:
+                populations[2023]['totale'] += population
 
+# visualisation des résultats
+annees = list(populations.keys())
+pop_immediate = [populations[annee]['immediate'] for annee in annees]
+pop_totale = [populations[annee]['totale'] for annee in annees]
 
-
-
+plt.plot(annees, pop_immediate, label='Population immédiate', marker='o')
+plt.plot(annees, pop_totale, label='Population totale', marker='o')
+plt.title('Évolution des populations (2008-2023)')
+plt.xlabel('Année')
+plt.ylabel('Population')
+plt.legend()
+plt.grid()
+plt.show()
